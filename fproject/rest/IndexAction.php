c20dc466dedfd051d4ae736a7b11750167745f78
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-03-10 17:38:49 +0700

    PK-13 Fix bug for release 0.6.0

--- fproject/rest/IndexAction.php
+++ fproject/rest/IndexAction.php
@@ -28,7 +28,7 @@ class IndexAction extends \yii\rest\IndexAction{
             $criteria = Json::decode($urlParams['criteria']);
             if(is_array($criteria) && isset($criteria['condition']))
             {
-                $params = is_array($criteria['params']) ? $criteria['params'] : [];
+                $params = isset($criteria['params']) && is_array($criteria['params']) ? $criteria['params'] : [];
 
                 /** @var ActiveQuery $query */
                 $query = Yii::createObject(ActiveQuery::className(), [$this->modelClass]);
