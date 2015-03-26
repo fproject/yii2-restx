3bed387f33457d83f64589f27f436e2620cefc08
Bui Sy Nguyen <nguyenbs@projectkit.net>
2015-03-26 18:07:45 +0700

    Fix bug FPRJ-67

--- fproject/rest/IndexAction.php
+++ fproject/rest/IndexAction.php
@@ -23,19 +23,27 @@ class IndexAction extends \yii\rest\IndexAction{
     {
         $dp = parent::prepareDataProvider();
         $urlParams = Yii::$app->getRequest()->queryParams;
-        if(is_array($urlParams) && isset($urlParams['criteria']))
+        if(is_array($urlParams))
         {
-            $criteria = Json::decode($urlParams['criteria']);
-            if(is_array($criteria) && isset($criteria['condition']))
+            if(isset($urlParams['criteria']))
             {
-                $params = isset($criteria['params']) && is_array($criteria['params']) ? $criteria['params'] : [];
+                $criteria = Json::decode($urlParams['criteria']);
+                if(is_array($criteria) && isset($criteria['condition']))
+                {
+                    $params = isset($criteria['params']) && is_array($criteria['params']) ? $criteria['params'] : [];
 
-                /** @var ActiveQuery $query */
-                $query = Yii::createObject(ActiveQuery::className(), [$this->modelClass]);
-                $query->where($criteria['condition'], $params);
-                $dp->query = $query;
+                    /** @var ActiveQuery $query */
+                    $query = Yii::createObject(ActiveQuery::className(), [$this->modelClass]);
+                    $query->where($criteria['condition'], $params);
+                    $dp->query = $query;
+                }
+            }
+            if(isset($urlParams['sort']))
+            {
+                $dp->getSort()->enableMultiSort = true;
             }
         }
+
         return $dp;
     }
 }
